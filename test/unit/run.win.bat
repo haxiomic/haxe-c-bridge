a41a7ad5a1eda2c72f4b38ff2e2716dc11d3b198
George Corney <haxiomic@gmail.com>
2022-01-17 18:17:28 +0000

    Debug info

--- test/unit/run.win.bat
+++ test/unit/run.win.bat
@@ -2,8 +2,11 @@
 haxe build-library.hxml
 
 @REM open this file in an x64 visual studio command prompt  
-cl .\app.c /I .\haxe-bin\ /link .\haxe-bin\obj\lib\Main-debug.lib
+cl .\app.c /I .\haxe-bin\ /Zi /link .\haxe-bin\obj\lib\Main-debug.lib /DEBUG
 @REM copy the dll locally
 copy haxe-bin\Main-debug.dll Main-debug.dll
+@REM copy the debugging info locally
+copy .\haxe-bin\obj\lib\Main-debug.pdb Main-debug.pdb
+
 @REM run app.exe
 app.exe
