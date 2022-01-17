06acf7188822655ac3cb052f8d024ef4a228ced3
George Corney <haxiomic@gmail.com>
2022-01-17 21:07:30 +0000

    Comments

--- test/unit/run.win.bat
+++ test/unit/run.win.bat
@@ -1,9 +1,12 @@
+@REM run this script in an x64 visual studio command prompt  
+
 @REM build haxe code
 haxe build-library.hxml || exit /b
 
-@REM open this file in an x64 visual studio command prompt  
+@REM compile app.c to app.exe
 cl .\app.c /I .\haxe-bin\ /Zi /link .\haxe-bin\obj\lib\Main-debug.lib /DEBUG || exit /b
-@REM copy the dll locally
+
+@REM copy the library dll locally for running
 copy haxe-bin\Main-debug.dll Main-debug.dll
 @REM copy the debugging info locally
 copy .\haxe-bin\obj\lib\Main-debug.pdb Main-debug.pdb
